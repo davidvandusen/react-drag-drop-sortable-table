777dbdc4d1d0ad0b0669f46b1b644637846f7ee0
David VanDusen <github@davidvandusen.com>
2016-11-26 12:33:45 -0800

    Build to /build/

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     './src/index.jsx'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/build/'
   },
