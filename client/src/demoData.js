6bec29e0d15421990aeb6325e5331346090c3777
David VanDusen <github@davidvandusen.com>
2016-11-26 08:54:55 -0800

    Load demo data from file.

---
+++ client/src/demoData.js
@@ -0,0 +1,71 @@
+export default {
+  ui: {
+    fieldOrder: [1, 2, 3],
+    itemOrder: [1, 2, 3],
+    draggedFieldId: null,
+    draggedItemId: null,
+    offsetX: 0,
+    offsetY: 0
+  },
+  // these are objectives
+  fields: [{
+    id: 1,
+    name: 'Field 1'
+  }, {
+    id: 2,
+    name: 'Field 2'
+  }, {
+    id: 3,
+    name: 'Field 3'
+  }],
+  // these are alternatives
+  items: [{
+    id: 1,
+    name: 'Item 1',
+    fieldData: [{
+      id: 1,
+      fieldId: 1,
+      value: 11
+    }, {
+      id: 2,
+      fieldId: 2,
+      value: 21
+    }, {
+      id: 3,
+      fieldId: 3,
+      value: 31
+    }]
+  }, {
+    id: 2,
+    name: 'Item 2',
+    fieldData: [{
+      id: 4,
+      fieldId: 1,
+      value: 12
+    }, {
+      id: 5,
+      fieldId: 2,
+      value: 22
+    }, {
+      id: 6,
+      fieldId: 3,
+      value: 32
+    }]
+  }, {
+    id: 3,
+    name: 'Item 3',
+    fieldData: [{
+      id: 7,
+      fieldId: 1,
+      value: 13
+    }, {
+      id: 8,
+      fieldId: 2,
+      value: 23
+    }, {
+      id: 9,
+      fieldId: 3,
+      value: 33
+    }]
+  }]
+};
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,78 +6,9 @@ import {Provider} from 'react-redux';
 import App from './App.jsx';
 import {createStore} from 'redux';
 import rootReducer from './reducers';
+import demoData from './demoData'
 
-const store = createStore(rootReducer, {
-  ui: {
-    fieldOrder: [1, 2, 3],
-    itemOrder: [1, 2, 3],
-    draggedFieldId: null,
-    draggedItemId: null,
-    offsetX: 0,
-    offsetY: 0
-  },
-  // these are objectives
-  fields: [{
-    id: 1,
-    name: 'Field 1'
-  }, {
-    id: 2,
-    name: 'Field 2'
-  }, {
-    id: 3,
-    name: 'Field 3'
-  }],
-  // these are alternatives
-  items: [{
-    id: 1,
-    name: 'Item 1',
-    fieldData: [{
-      id: 1,
-      fieldId: 1,
-      value: 11
-    }, {
-      id: 2,
-      fieldId: 2,
-      value: 21
-    }, {
-      id: 3,
-      fieldId: 3,
-      value: 31
-    }]
-  }, {
-    id: 2,
-    name: 'Item 2',
-    fieldData: [{
-      id: 4,
-      fieldId: 1,
-      value: 12
-    }, {
-      id: 5,
-      fieldId: 2,
-      value: 22
-    }, {
-      id: 6,
-      fieldId: 3,
-      value: 32
-    }]
-  }, {
-    id: 3,
-    name: 'Item 3',
-    fieldData: [{
-      id: 7,
-      fieldId: 1,
-      value: 13
-    }, {
-      id: 8,
-      fieldId: 2,
-      value: 23
-    }, {
-      id: 9,
-      fieldId: 3,
-      value: 33
-    }]
-  }]
-});
+const store = createStore(rootReducer, demoData);
 
 var root = document.createElement('div');
 document.body.appendChild(root);
